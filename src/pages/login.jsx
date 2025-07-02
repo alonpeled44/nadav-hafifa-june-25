7199b36201da5a855a2773a5c605e6c273e2f20f
njTheNudev <nad.jacobsohn@gmail.com>
2025-07-02 14:16:46 +0300

    add login and login as guest buttons functionality

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,40 @@
 import styles from "@/styles/pages/login.module.css";
+import { useState } from "react";
+
+const userList = [
+  { user: "a", pass: "b" },
+  { user: "testUser", pass: "testPass" },
+];
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const foundUser = userList.find(({ user }) => user === username);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (foundUser) {
+      if (foundUser.pass === password) {
+        alert("welcome, " + username);
+        location.reload();
+      } else {
+        setErrorMessage("username and password do not match");
+      }
+    } else {
+      setErrorMessage("username not found");
+    }
+  };
+
+  const handleGuestLogin = () => {
+    alert("welcome, guest");
+    location.reload();
+  };
+
   return (
     <div className={styles["form-wrapper"]}>
-      <form className={styles["login-form"]}>
+      <form className={styles["login-form"]} onSubmit={handleSubmit}>
         <div className={styles["logo-phone"]}>
           <h1>pokédex</h1>
 
@@ -18,6 +50,8 @@ export default function Login() {
             className="username"
             placeholder="username"
             required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
 
           <input
@@ -26,16 +60,22 @@ export default function Login() {
             className="password"
             placeholder="password"
             required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
-          <span className={styles.error}></span>
+          <span className={styles.error}>{errorMessage}</span>
         </div>
 
         <div className={styles.buttons}>
           <button type="submit" className={styles["user-login"]}>
             log in
           </button>
-          <button type="submit" className={styles["guest-login"]}>
+          <button
+            type="button"
+            className={styles["guest-login"]}
+            onClick={handleGuestLogin}
+          >
             join as guest
           </button>
         </div>
