f94c9bf63e6feeb72328e691be6e949031126866
njTheNudev <nad.jacobsohn@gmail.com>
2025-07-06 14:45:05 +0300

    add username and logout button to header + make them appear only when user signed in + correctly show "guest" when logged in as guest

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,28 @@ export default function Header() {
   const now = new Date();
   const localDate = now.toLocaleDateString("en-UK");
 
+  let displayName = "";
+  if (localStorage.getItem("currentUser") === "!") {
+    displayName = "guest";
+  } else {
+    displayName = localStorage.getItem("currentUser");
+  }
+
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src="/pokeball.png" />
-        <h1>pokémon</h1>
+      <div className={styles["header-left-part"]}>
+        <div className={styles.logo}>
+          <img src="/pokeball.png" />
+          <h1>pokémon</h1>
+        </div>
+
+        {localStorage.getItem("currentUser") && <span> | </span>}
+        {localStorage.getItem("currentUser") && (
+          <div className={styles["username-and-logout"]}>
+            <p>{displayName}</p>
+            <button className={styles.logout}> sign out </button>
+          </div>
+        )}
       </div>
 
       <p className={styles["display-date"]}>{localDate}</p>
