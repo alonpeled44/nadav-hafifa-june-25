4299b8726c536925ee606603109e6b5636b7cd88
njTheNudev <nad.jacobsohn@gmail.com>
2025-07-06 15:23:57 +0300

    added title and styling to main page on phone

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,50 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "@/styles/pages/home.module.css";
 
 export default function Home() {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [displayName, setDisplayName] = useState("");
+
   const router = useRouter();
+
   useEffect(() => {
-    if (!localStorage.getItem("currentUser")) {
+    const user = localStorage.getItem("currentUser");
+    if (!user) {
       router.push("/login");
+    } else if (user === "!") {
+      setDisplayName("guest");
+    } else {
+      setDisplayName(user);
     }
+  }, []);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <div className={styles["home-page-wrapper"]}>
+      {windowWidth <= 1200 && (
+        <div className={styles["phone-title"]}>
+          <div className={styles.logo}>
+            <h1>pokédex</h1>
+            <img src="/pokeball.png" />
+          </div>
+
+          <div className={styles["username-and-logout"]}>
+            <p>{displayName}</p>
+            <button className={styles.logout}> sign out </button>
+          </div>
+        </div>
+      )}
       Hello Worldasdfasdf
       <Link href="/login">bruhbruhfb</Link>
     </div>
